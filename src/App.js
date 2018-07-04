fae4dd461b4df0e294b84630adea3bb78560e97a
upen1310 <upendra.pearl@gmail.com>
2018-07-04 05:40:55 +0000

    App.js edited online with Bitbucket

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Routing from "./components/Routing";
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <Header />
+        <Routing />
         <Footer />
       </div>
     );
