59de0a8aedb99b4fa9697178c2c848473e8d0e65
upen1310 <upendra.pearl@gmail.com>
2018-07-04 05:40:22 +0000

    Header.js edited online with Bitbucket

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import {
     Collapse,
     Navbar,
@@ -9,12 +8,6 @@ import {
     NavItem,
     NavLink
 } from "reactstrap";
-
-import Body from "./Body";
-import Country from "./Country";
-import Category from "./Category";
-import CountriesJSON from "../json/countries.json";
-import CategoriesJSON from "../json/categories.json";
 
 class Header extends React.Component {
     constructor(props) {
@@ -44,30 +37,12 @@ class Header extends React.Component {
                             <NavItem>
                                 <NavLink href="/category">Category</NavLink>
                             </NavItem>
+                            <NavItem>
+                                <NavLink href="/about">About-me</NavLink>
+                            </NavItem>
                         </Nav>
                     </Collapse>
                 </Navbar>
-                <Router>
-                    <Switch>
-                        <Route exact path="/"  key="in" render={props => <Body paramType="country" param="in" />} />
-                        <Route path="/country" component={Country} />
-                        <Route path="/category" component={Category} />
-                        {
-                            CountriesJSON.map((country, index) => {
-                                return (
-                                    <Route path={"/"+country.code}  key={country.code} render={props => <Body {...props} paramType="country" param={country.code} />} />
-                                )
-                            })
-                        }
-                        {
-                            CategoriesJSON.map((category, index) => {
-                                return (
-                                    <Route path={"/"+category}  key={category} render={props => <Body {...props} paramType="category" param={category} />} />
-                                )
-                            })
-                        }
-                    </Switch>
-                </Router>
             </div>
         );
     }
